84e42b2391abec1d44a69f582d32b9033751cd47
TongThanaphon <tongasura001@gmail.com>
2023-03-09 23:43:22 +0700

    update: add tailwindcss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<p class="text-red-700">Style with tailwindcss</p>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
+</style>
